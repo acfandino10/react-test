f716df60a8bd80b36c4324b1fada9349c84e2c1e
Ana <ana.fandino@itwox.com>
2023-11-09 21:28:09 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
   // distDir: 'dist',
   env: {
     REACT_APP_BASE_URL: process.env.REACT_APP_BASE_URL,
+    REACT_APP_TOKEN: process.env.REACT_APP_TOKEN,
   },
   images: {
     domains: ["remotewoman.com", "cdn.logo.com", "adobe.com"],
